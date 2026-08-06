f0b30241b51848606eeefbd858d229f7d5f4e887
Vinay Parit <vinaypparit@gmail.com>
2026-08-06 23:28:13 +0530

    script.js updated

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -256,7 +256,7 @@ completedBtn.addEventListener("click", () => {
 mobileAllBtn.addEventListener("click", () => {
   let allTodos = document.querySelectorAll(".todoItem");
   allTodos.forEach((todo) => {
-    todo.style.display = "none";
+    todo.style.display = "flex";
   });
 });
 
